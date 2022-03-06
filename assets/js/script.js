abd17e13db4f22b7585fd835dabd5d4beef9f759
lukaszkukla <lukaszkukla@gmail.com>
2022-03-06 17:52:33 +0000

    refactor: update fetch call

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,25 +30,28 @@ const endGameRef = document.querySelector('.end-game');
 nextQuestionRef.addEventListener('click', createQuestion); // advance to the next quetion
 restartRef.addEventListener('click', restartQuiz); // restart the quiz
 
-fetch(APIURL) // fetch url from opentdb.com
-    .then(function (response) {
-        console.log('success');
-        return response.json(); // return data in json format    
-    })
-    .then(function (data) {
-        console.log(data.results); // test log to console 
-        quiz.totalQuestions = data.results.length; // total number of question fetched from opentdb.com
-        console.log(quiz.totalQuestions); // test log to console 
-        quiz.questionNumber = 0; // starting question / questions we are on
-        quiz.score = 0;
-        console.log(quiz.questionNumber); // test log to console 
-        quiz.array = data.results;
-        quiz.array.forEach(function(element) {
-            console.log(element); // test log to console 
-        });
 
-        createQuestion();
-    })
+/** 
+* fetch questions from opentdb.com
+*/
+function getQuestions(APIURL) {
+    fetch(APIURL)
+        .then(response => response.json())
+        .then((data) => questionsBank(data))
+        .then(() => createQuestion());
+}
+
+getQuestions(APIURL)
+
+/** 
+* pass fetched questions to questionBank
+*/
+
+function questionsBank(data) {  
+    quiz.totalQuestions = data.results.length;  
+    quiz.questionBank = data.results;
+    return;
+}
 
 // determine behaviour based on the current question number
 function createQuestion() {
@@ -59,7 +62,7 @@ function createQuestion() {
     } else {
         console.log(`question # ${quiz.questionNumber + 1} out of ${quiz.totalQuestions}`); // test log to console
         
-        let q = quiz.array[quiz.questionNumber];
+        let q = quiz.questionBank[quiz.questionNumber];
         let answerOptions = [q.correct_answer, ...q.incorrect_answers]; // combine correct and incorrect answers into one array        
         let shuffleAnswers = answerOptions.sort(() => Math.random - 0.5); // randomise answer order
         
